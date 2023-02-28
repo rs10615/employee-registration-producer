09e81b6936e1cc5d23999f637bd7a77e6e7484a8
rahul sharma <31204067+rs10615@users.noreply.github.com>
2023-02-28 20:16:55 +0530

    initial changes

---
+++ employee-registration-producer/src/main/java/com/sample/code/controller/EmployeeRegistrationController.java
@@ -0,0 +1,28 @@
+package com.sample.code.controller;
+
+import com.sample.code.source.EmployeeRegistrationSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.integration.support.MessageBuilder;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.sample.code.model.Employee;
+
+@RestController
+@EnableBinding(EmployeeRegistrationSource.class)
+public class EmployeeRegistrationController {
+
+	@Autowired
+	EmployeeRegistrationSource employeeRegistrationSource;
+
+	@RequestMapping("/register")
+	@ResponseBody
+	public String orderFood(@RequestBody Employee employee) {
+		employeeRegistrationSource.employeeRegistration().send(MessageBuilder.withPayload(employee).build());
+		System.out.println(employee.toString());
+		return "Employee Registered";
+	}
+}
---
+++ employee-registration-producer/src/main/java/com/sample/code/source/EmployeeRegistrationSource.java
@@ -0,0 +1,11 @@
+package com.sample.code.source;
+
+import org.springframework.cloud.stream.annotation.Output;
+import org.springframework.messaging.MessageChannel;
+
+public interface EmployeeRegistrationSource {
+
+	@Output("employeeRegistrationChannel")
+	MessageChannel employeeRegistration();
+
+}
